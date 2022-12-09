9d1c6a09e5c24f7a6a5f7a52177b71836c6d08c6
hugovarellaa <hugovarellaa@gmail.com>
2022-12-09 18:09:25 -0300

    BasePageLayout reponsive

--- src/shared/layouts/BasePageLayout.tsx
+++ src/shared/layouts/BasePageLayout.tsx
@@ -9,15 +9,21 @@ import { Box } from '@mui/system'
 import { ReactNode } from 'react'
 import { useDrawerContext } from '../context'
 
-interface BasePageLayoutProps {
-  children: ReactNode
+interface IBasePageLayoutProps {
   title: string
+  children: ReactNode
+  toolbar?: ReactNode
 }
 
-export function BasePageLayout({ children, title }: BasePageLayoutProps) {
+export function BasePageLayout({
+  children,
+  title,
+  toolbar,
+}: IBasePageLayoutProps) {
   const { toggleDrawerOpen } = useDrawerContext()
   const theme = useTheme()
   const smDown = useMediaQuery(theme.breakpoints.down('sm'))
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'))
 
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1}>
@@ -25,17 +31,26 @@ export function BasePageLayout({ children, title }: BasePageLayoutProps) {
         display="flex"
         alignItems="center"
         padding={1}
-        height={theme.spacing(12)}>
+        height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}>
         {smDown && (
           <IconButton onClick={toggleDrawerOpen}>
             <Icon>menu</Icon>
           </IconButton>
         )}
 
-        <Typography variant="h5">{title}</Typography>
+        <Typography
+          variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="ellipsis">
+          {title}
+        </Typography>
       </Box>
-      <Box>Barra de ferramentas</Box>
-      <Box>{children}</Box>
+      {toolbar && <Box>{toolbar}</Box>}
+
+      <Box flex={1} overflow="auto">
+        {children}
+      </Box>
     </Box>
   )
 }

2d27f88063f612174c6e3d4e9f459e67c2930c94
hugovarellaa <hugovarellaa@gmail.com>
2022-12-10 22:31:12 -0300

    Loading table

--- src/pages/customers/Customers.tsx
+++ src/pages/customers/Customers.tsx
@@ -4,14 +4,15 @@ import { Toolbar } from '../../shared/components'
 import { useDebounce } from '../../shared/hooks'
 import { BasePageLayout } from '../../shared/layouts/BasePageLayout'
 import { customerServices, ICustomers } from '../../shared/utils/methods'
-
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
+import TableFooter from '@mui/material/TableFooter'
 import Paper from '@mui/material/Paper'
+import LinearProgress from '@mui/material/LinearProgress'
 
 export function Customers() {
   const [rows, setRows] = useState<ICustomers[]>([])
@@ -63,9 +64,9 @@ export function Customers() {
             <TableHead>
               <TableRow>
                 <TableCell>Ações</TableCell>
-                <TableCell align="right">First name</TableCell>
-                <TableCell align="right">Last name</TableCell>
-                <TableCell align="right">Email</TableCell>
+                <TableCell>First name</TableCell>
+                <TableCell>Last name</TableCell>
+                <TableCell>Email</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -74,12 +75,19 @@ export function Customers() {
                   <TableCell component="th" scope="row">
                     Ações
                   </TableCell>
-                  <TableCell align="right">{row.firstName}</TableCell>
-                  <TableCell align="right">{row.lastName}</TableCell>
-                  <TableCell align="right">{row.email}</TableCell>
+                  <TableCell>{row.firstName}</TableCell>
+                  <TableCell>{row.lastName}</TableCell>
+                  <TableCell>{row.email}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
+            <TableFooter>
+              {isLoading && (
+                <TableCell colSpan={4}>
+                  <LinearProgress variant="indeterminate" />
+                </TableCell>
+              )}
+            </TableFooter>
           </Table>
         </TableContainer>
       </BasePageLayout>

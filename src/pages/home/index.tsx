9a5d98e378b449412faf025025329aaa14fa82a0
hugo <hugovarellaa@gmail.com>
2023-01-13 17:50:18 -0300

    fix: structure home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,28 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { HomeContainer, HomeHero, HomePreview } from './styles'
+
+import Image from 'next/image'
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <HomeHero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="lg">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </HomeHero>
+      <HomePreview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando a aplicação em funcionamento"
+        />
+      </HomePreview>
+    </HomeContainer>
+  )
+}
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from '@ignite-ui/react'
+
+export const HomeContainer = styled('div', {})
+
+export const HomeHero = styled('div', {})
+
+export const HomePreview = styled('div', {})

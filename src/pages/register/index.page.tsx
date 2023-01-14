b715ab1db9032d8c67c18d0f2ef1df8731717614
hugo <hugovarellaa@gmail.com>
2023-01-14 15:34:32 -0300

    fix: Create Layout connect-calendar page

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -44,6 +44,7 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+      await router.push('/register/connect-calendar')
     } catch (error) {
       if (error instanceof AxiosError && error?.response?.data?.mesage) {
         return alert(error.response?.data?.mesage)

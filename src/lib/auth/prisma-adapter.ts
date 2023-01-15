21a0248e6a15130b33c8a9bb661f848d15894cca
hugo <hugovarellaa@gmail.com>
2023-01-15 11:49:56 -0300

    fix Method updateSession

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -132,7 +132,23 @@ export function PrismaAdapter(): Adapter {
         },
       }
     },
-    async updateSession({ sessionToken }) {},
+
+    async updateSession({ sessionToken, userId, expires }) {
+      const prismaSession = await prisma.session.update({
+        where: {
+          session_token: sessionToken,
+        },
+        data: {
+          expires,
+          user_id: userId,
+        },
+      })
+      return {
+        sessionToken: prismaSession.session_token,
+        userId: prismaSession.user_id,
+        expires: prismaSession.expires,
+      }
+    },
     async createVerificationToken({ identifier, expires, token }) {},
     async useVerificationToken({ identifier, token }) {},
   }
